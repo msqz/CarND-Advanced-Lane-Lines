fdb634ada753614e1478b1693d5f104a6f03b509
msqz <ms.skuza@gmail.com>
2018-09-22 18:33:42 +0200

    Cleaned up

--- drawer.py
+++ drawer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import transformation
 
 
 def draw_text(img, paragraphs):
@@ -17,13 +18,8 @@ def draw_text(img, paragraphs):
                     cv2.LINE_AA)
 
 
-def draw_lane(img, left_fitx, right_fitx, ploty, orig, M):
-    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
-    pts_right = np.array([
-        np.flipud(np.transpose(np.vstack([right_fitx, ploty])))
-    ])
-
-    pts = np.hstack((pts_left, pts_right))
+def extend_canvas(pts):
+    # --- 58 ms ---
     pts_ext = np.copy(pts)
     pts_ext[0, :, 0] += 1280
     pts_ext[0, :, 1] += 720
@@ -32,32 +28,57 @@ def draw_lane(img, left_fitx, right_fitx, ploty, orig, M):
     lines_ext = np.zeros((color_warp_ext.shape[0], color_warp_ext.shape[1]))
     cv2.polylines(lines_ext, np.int_([pts_ext]), False, (255, 0, 0), 15)
     lines_ext = lines_ext[720:-720, 1100:-1100]
-    src = np.copy(warping_to)
+    return color_warp_ext, lines_ext
+
+
+def unwarp_extended(color_warp_ext):
+    ''' Extended matrix is now transformed back to be in perspective
+    '''
+    # --- 116 ms ---
+    src = np.copy(transformation.warping_to)
     src[:, 0] += 1280
     src[:, 1] += 720
-    dst = np.float32([[200, 720], [604, 450], [696, 450], [1120, 720]])
-    dst = np.copy(warping_from)
+    dst = np.copy(transformation.warping_from)
     dst[:, 0] += 1280
     dst[:, 1] += 720
     M_ext = cv2.getPerspectiveTransform(src, dst)
 
-    new_warp_ext = cv2.warpPerspective(
+    return cv2.warpPerspective(
         color_warp_ext, M_ext, (1280*3, 720*3))
-    new_warp_ext = new_warp_ext[720:-720, 1280:-1280]
+
+
+def crop_to_original(new_warp_ext):
+    return new_warp_ext[720:-720, 1280:-1280]
+
+
+def draw_lane(lane, orig):
+    pts_left = np.array(
+        [np.transpose(np.vstack([lane.left_fitx, lane.ploty]))])
+    pts_right = np.array([
+        np.flipud(np.transpose(np.vstack([lane.right_fitx, lane.ploty])))
+    ])
+    pts = np.hstack((pts_left, pts_right))
+
+    color_warp_ext, lines_ext = extend_canvas(pts)
+
+    new_warp_ext = unwarp_extended(color_warp_ext)
+
+    new_warp_ext = crop_to_original(new_warp_ext)
+
     weighted = cv2.addWeighted(orig, 1, new_warp_ext, 0.3, 0)
+
     return weighted, lines_ext
 
 
-def combine(img, warped, binary, left_curverad, right_curverad, position):
+def combine(img, warped, binary, curverad, position):
     warped_sm = cv2.resize(warped, (320, 180))
     img[5:185, -325:-5] = np.dstack((warped_sm, warped_sm, warped_sm))
     binary_sm = cv2.resize(binary, (320, 180))
     img[5:185, -650:-330] = np.dstack((binary_sm, binary_sm, binary_sm))
 
     draw_text(img, [
-        'Radius left: {}'.format(left_curverad),
-        'Radius right: {}'.format(right_curverad),
-        'Position: {}'.format(position),
+        'Radius: {}m'.format(curverad),
+        'Position: {}m'.format(position),
     ])
 
     return img
--- main.py
+++ main.py
@@ -15,102 +15,53 @@ import convolution
 import helpers
 import transformation
 import drawer
+from lane import Lane
 
-profile = False
-t = time.clock()
-
-mtx = None
-dist = None
+mtx, dist = transformation.calibrate()
 frame_no = 0
-left_fit = None
-right_fit = None
-ploty = None
-
-warping_from = np.float32([[200, 720], [604, 450], [696, 450], [1120, 720]])
-warping_to = np.float32([[200, 720], [200, 0], [1120, 0], [1120, 720]])
-
-
-def click_stopwatch(action):
-    global t
-    measured = round(time.clock() - t, 2) * 1000
-    print('{}: {}'.format(action, measured))
-    t = time.clock()
-
-
-def warp(img):
-    M = cv2.getPerspectiveTransform(warping_from, warping_to)
-    return cv2.warpPerspective(img, M, (1280, 720)), M
-
-
-def draw_lane(img, left_fitx, right_fitx, ploty, orig, M):
-    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
-    pts_right = np.array([
-        np.flipud(np.transpose(np.vstack([right_fitx, ploty])))
-    ])
-
-    pts = np.hstack((pts_left, pts_right))
-    pts_ext = np.copy(pts)
-    pts_ext[0, :, 0] += 1280
-    pts_ext[0, :, 1] += 720
-    color_warp_ext = np.zeros((720*3, 1280*3, 3), dtype=np.uint8)
-    cv2.fillPoly(color_warp_ext, np.int_([pts_ext]), (0, 255, 0))
-    lines_ext = np.zeros((color_warp_ext.shape[0], color_warp_ext.shape[1]))
-    cv2.polylines(lines_ext, np.int_([pts_ext]), False, (255, 0, 0), 15)
-    lines_ext = lines_ext[720:-720, 1100:-1100]
-    # src = np.float32([[233, 720], [233, 0], [1087, 0], [1087, 720]])
-    # src = np.float32([[200, 720], [200, 0], [1120, 0], [1120, 720]])
-    src = np.copy(warping_to)
-    src[:, 0] += 1280
-    src[:, 1] += 720
-    dst = np.float32([[200, 720], [604, 450], [696, 450], [1120, 720]])
-    dst = np.copy(warping_from)
-    dst[:, 0] += 1280
-    dst[:, 1] += 720
-    M_ext = cv2.getPerspectiveTransform(src, dst)
-
-    new_warp_ext = cv2.warpPerspective(
-        color_warp_ext, M_ext, (1280*3, 720*3))
-    new_warp_ext = new_warp_ext[720:-720, 1280:-1280]
-    weighted = cv2.addWeighted(orig, 1, new_warp_ext, 0.3, 0)
-    return weighted, lines_ext
 
 
 def pipeline(img):
-    global left_fit
-    global right_fit
+
     undistorted = transformation.undistort(img, mtx, dist)
+
     binary = threshold.to_binary(undistorted)
-    warped, M = warp(binary)
-    left_fitx, right_fitx, p, l_fit, r_fit = lanes.fit_polynomial(warped)
-    left_fit = l_fit
-    right_fit = r_fit
-    ploty = p
-    left_curverad, right_curverad = geometry.determine_curvature(
-        left_fitx, right_fitx, ploty)
-    position = geometry.determine_position(left_fitx, right_fitx)
-    lane, lines = draw_lane(
-        warped, left_fitx, right_fitx, ploty, undistorted, M)
-    return lane, lines, binary * 255, left_curverad, right_curverad, position
+
+    warped, M = transformation.warp(binary)
+
+    lane = Lane(lambda: lanes.fit_polynomial(warped))
+
+    curverad = lane.get_curverad()
+
+    position = lane.get_position()
+
+    lane, lines = drawer.draw_lane(lane, undistorted)
+
+    return lane, lines, binary * 255, curverad, position
 
 
-mtx, dist = transformation.calibrate()
+def photo():
+    image = np.asarray(Image.open(sys.argv[1]))[:, :, :3]
+    combined = drawer.combine(*pipeline(image))
+    helpers.show(combined)
+
+
+def video():
+    reader = skvideo.io.FFmpegReader(sys.argv[1])
+    writer = skvideo.io.FFmpegWriter("/home/m/Videos/output.mp4")
+    for frame in reader.nextFrame():
+        combined = drawer.combine(*pipeline(frame))
+        writer.writeFrame(combined)
+        frame_no += 1
+
+    reader.close()
+    writer.close()
+
 
 if len(sys.argv) != 2:
     raise Exception('missing path')
 
 if sys.argv[1][-4:] == ".mp4":
-    reader = skvideo.io.FFmpegReader(sys.argv[1])
-    writer = skvideo.io.FFmpegWriter("/home/m/Videos/output.mp4")
-    for frame in reader.nextFrame():
-        profile and click_stopwatch('Frame started')
-        combined = drawer.combine(*pipeline(frame))
-        writer.writeFrame(combined)
-        frame_no += 1
-        profile and click_stopwatch('Frame ended')
-
-    reader.close()
-    writer.close()
+    video()
 else:
-    image = np.asarray(Image.open(sys.argv[1]))[:, :, :3]
-    combined = drawer.combine(*pipeline(image))
-    helpers.show(combined)
+    photo()
